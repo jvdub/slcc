a7e08a94b543205e30407daca7579bc4a08069de
John Van Wagenen <jvanwagenen@mastercontrol.com>
2020-05-05 19:38:08 -0600

    JS Weeks 2 activity

--- resume/js/myJavaScript.js
+++ resume/js/myJavaScript.js
@@ -1,3 +1,28 @@
+function customizeGreeting() {
+    let hour = getTimeOfDay();
+    let message = 'Early Bird Special';
+
+    if (hour >= 5 && hour < 12) {
+        message = 'Good Morning';
+    } else if (hour >= 12 && hour < 18) {
+        message = 'Good Afternoon';
+    } else if (hour >= 18) {
+        message = 'Good Evening and Good Night';
+    }
+
+    displayGreetingMessage(message);
+}
+
+function displayGreetingMessage(message) {
+    document.getElementById('message').innerHTML = message;
+}
+
+function getTimeOfDay() {
+    let now = new Date();
+    let hour = now.getHours();
+    return hour;
+}
+
 function greetUser() {
     let name = document.getElementById('greetingName').value;
     document.getElementById('userName').innerHTML = name;
@@ -7,6 +32,7 @@ function greetUser() {
 
 $(document).ready(() => {
     $('#getUserName').modal('show');
+    customizeGreeting();
 });
 
 function greetings() {
